6b6a1ad84e41541c2f17e58b9f1803f5ed75edaf
Roy Schrauwen <roy@royschrauwen.nl>
2022-10-07 06:55:05 +0200

    Added OfferRepository

--- src/main/java/avd/inf/jdm/rentmycar/repository/OfferRepository.java
+++ src/main/java/avd/inf/jdm/rentmycar/repository/OfferRepository.java
@@ -1,4 +1,17 @@
 package avd.inf.jdm.rentmycar.repository;
 
-public class OfferRepository {
+import avd.inf.jdm.rentmycar.domain.Offer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface OfferRepository extends JpaRepository<Offer, Long> {
+
+    List<Offer> findOffersByPickupLocation(String city);
+
+    // TODO RS: Check if this works after inplementing the bookings-subsystem
+    @Query(value = "SELECT * FROM offers AS o WHERE o.id NOT EXISTS (SELECT offer FROM bookings)", nativeQuery = true)
+    List<Offer> findUnbooked();
+
 }

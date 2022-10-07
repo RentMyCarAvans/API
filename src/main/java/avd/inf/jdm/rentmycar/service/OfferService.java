c6b12d6fdf6bca041666cefa3c68cc8510d57133
Roy Schrauwen <roy@royschrauwen.nl>
2022-10-07 07:02:59 +0200

    Added OfferService

--- src/main/java/avd/inf/jdm/rentmycar/service/OfferService.java
+++ src/main/java/avd/inf/jdm/rentmycar/service/OfferService.java
@@ -1,4 +1,38 @@
 package avd.inf.jdm.rentmycar.service;
 
+import avd.inf.jdm.rentmycar.domain.Offer;
+import avd.inf.jdm.rentmycar.repository.OfferRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class OfferService {
+    private final OfferRepository offerRepository;
+    @Autowired
+    public OfferService(OfferRepository offerRepository) {
+        this.offerRepository = offerRepository;
+    }
+
+    public List<Offer> getAll() {
+        return offerRepository.findAll();
+    }
+
+    public Optional<Offer> getSingleById(Long id) {
+        return offerRepository.findById(id);
+    }
+
+    public List<Offer> getOffersByPickupLocation(String city) {
+        return offerRepository.findOffersByPickupLocation(city);
+    }
+
+    public List<Offer> getUnbooked() {
+        return offerRepository.findUnbooked();
+    }
+
+    public Offer create(Offer offer) {
+        return offerRepository.save(offer);
+    }
 }

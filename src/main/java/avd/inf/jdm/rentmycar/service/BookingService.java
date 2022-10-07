a7675e958e0e7dbd24aa2d15336d2d6389bdc38e
Roy Schrauwen <roy@royschrauwen.nl>
2022-10-07 11:03:02 +0200

    Created BookingService

--- src/main/java/avd/inf/jdm/rentmycar/service/BookingService.java
+++ src/main/java/avd/inf/jdm/rentmycar/service/BookingService.java
@@ -1,4 +1,47 @@
 package avd.inf.jdm.rentmycar.service;
 
-public class BookingService {
+import avd.inf.jdm.rentmycar.domain.Booking;
+import avd.inf.jdm.rentmycar.domain.Offer;
+import avd.inf.jdm.rentmycar.domain.User;
+import avd.inf.jdm.rentmycar.repository.BookingRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+class BookingService {
+
+    private final BookingRepository bookingRepository;
+
+    @Autowired
+    public BookingService(BookingRepository bookingRepository) {
+        this.bookingRepository = bookingRepository;
+    }
+
+    public List<Booking> getAll() {
+        return bookingRepository.findAll();
+    }
+
+    public Optional<Booking> getSingleById(Long id) {
+        return bookingRepository.findById(id);
+    }
+
+    public List<Booking> getBookingsByCustomer(User customer) {
+        return bookingRepository.findBookingsByCustomer(customer);
+    }
+
+    public Optional<Booking> getBookingByOffer(Offer offer) {
+        return bookingRepository.findBookingByOffer(offer);
+    }
+
+    public Booking create(Booking booking) {
+        return bookingRepository.save(booking);
+    }
+
+    public Booking save(Booking booking) {
+        return bookingRepository.save(booking);
+    }
+
 }

3e2c2a726d6b96382b98516b4d1b44429212e6ca
Roy Schrauwen <roy@royschrauwen.nl>
2022-10-07 07:07:52 +0200

    Added OfferController

--- src/main/java/avd/inf/jdm/rentmycar/controller/OfferController.java
+++ src/main/java/avd/inf/jdm/rentmycar/controller/OfferController.java
@@ -1,4 +1,48 @@
 package avd.inf.jdm.rentmycar.controller;
 
+import avd.inf.jdm.rentmycar.domain.Offer;
+import avd.inf.jdm.rentmycar.service.OfferService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/v1/offers")
 public class OfferController {
+
+    private final OfferService offerService;
+    @Autowired
+    public OfferController(OfferService offerService) {
+        this.offerService = offerService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Offer>> getAllCourses(@RequestParam(required = false) String city){
+        List<Offer> found = city == null ? offerService.getAll() : offerService.getOffersByPickupLocation(city);
+        return found.isEmpty() ? ResponseEntity.noContent().build() : ResponseEntity.ok(found);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Offer>> getById(@PathVariable Long id){
+        Optional<Offer> found =  offerService.getSingleById(id);
+        return found.isEmpty() ? ResponseEntity.noContent().build() : ResponseEntity.ok(found);
+    }
+
+    // TODO RS: Check if this works after inplementing the bookings-subsystem
+    @GetMapping("/unbooked")
+    public ResponseEntity<List<Offer>> getAllUnbookedOffers() {
+        try {
+            List<Offer> found = new ArrayList<>();
+            found.addAll(offerService.getUnbooked());
+            return found.isEmpty() ? ResponseEntity.noContent().build() : ResponseEntity.ok(found);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
 }

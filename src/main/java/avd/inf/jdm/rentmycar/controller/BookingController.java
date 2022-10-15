5944c54d949c5da59e9c3fc12e97cba10a36b70a
Roy Schrauwen <roy@royschrauwen.nl>
2022-10-15 09:34:04 +0200

    [45] BookingDTO implemented

--- src/main/java/avd/inf/jdm/rentmycar/controller/BookingController.java
+++ src/main/java/avd/inf/jdm/rentmycar/controller/BookingController.java
@@ -1,8 +1,13 @@
 package avd.inf.jdm.rentmycar.controller;
 
+import avd.inf.jdm.rentmycar.ResponseHandler;
+import avd.inf.jdm.rentmycar.controller.dto.BookingDTO;
 import avd.inf.jdm.rentmycar.domain.Booking;
 import avd.inf.jdm.rentmycar.domain.Offer;
+import avd.inf.jdm.rentmycar.domain.User;
 import avd.inf.jdm.rentmycar.service.BookingService;
+import avd.inf.jdm.rentmycar.service.OfferService;
+import avd.inf.jdm.rentmycar.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,31 +21,47 @@ import java.util.Optional;
 public class BookingController {
 
     private final BookingService bookingService;
+    private final OfferService offerService;
+    private final UserService userService;
 
     @Autowired
-    public BookingController(BookingService bookingService) {
+    public BookingController(BookingService bookingService, OfferService offerService, UserService userService) {
         this.bookingService = bookingService;
+        this.offerService = offerService;
+        this.userService = userService;
     }
 
     @GetMapping("/v1/bookings")
-    public ResponseEntity<List<Booking>> getAllBookings(){
+    public ResponseEntity<Object> getAllBookings(){
         List<Booking> found =  bookingService.getAll();
-        return found.isEmpty() ? ResponseEntity.noContent().build() : ResponseEntity.ok(found);
+        return found.isEmpty()
+                ? ResponseHandler.generateResponse("No bookings found", HttpStatus.NO_CONTENT, null)
+                : ResponseHandler.generateResponse(null, HttpStatus.OK, found);
     }
 
     @GetMapping("/v1/bookings/{id}")
-    public ResponseEntity<Optional<Booking>> getById(@PathVariable Long id){
+    public ResponseEntity<Object> getById(@PathVariable Long id){
         Optional<Booking> found =  bookingService.getSingleById(id);
-        return found.isEmpty() ? ResponseEntity.noContent().build() : ResponseEntity.ok(found);
+        return found.isEmpty()
+                ? ResponseHandler.generateResponse("Booking with id " + id + " not found", HttpStatus.NOT_FOUND, null)
+                : ResponseHandler.generateResponse(null, HttpStatus.OK, found);
     }
 
     @PostMapping("/v1/bookings")
-    public ResponseEntity<Booking> create(@RequestBody Booking newBooking){
+    public ResponseEntity<Object> create(@RequestBody BookingDTO bookingDTO){
         try {
-            Booking booking = bookingService.save(newBooking);
-            return new ResponseEntity<>(booking, HttpStatus.CREATED);
+            Offer offer = offerService.getSingleById(bookingDTO.getOfferId()).get();
+            User customer = userService.getUserByID(bookingDTO.getCustomerId());
+            Booking newBooking = bookingService.create(offer, customer);
+
+            if (newBooking != null) {
+                return new ResponseHandler().generateResponse("Booking created", HttpStatus.CREATED, newBooking);
+            }
+
+            return new ResponseHandler().generateResponse("Booking could not be created", HttpStatus.BAD_REQUEST, null);
+
         } catch (IllegalArgumentException e) {
-            return ResponseEntity.badRequest().build();
+            return ResponseHandler.generateResponse(e.getMessage(), HttpStatus.BAD_REQUEST, null);
         }
     }
 

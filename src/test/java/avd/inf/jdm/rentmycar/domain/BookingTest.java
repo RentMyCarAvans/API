170e09d83da1133a60fb28cfad19d4ad56dc26e5
Roy Schrauwen <roy@royschrauwen.nl>
2022-10-15 09:35:01 +0200

    Create BookingTest.java

---
+++ src/test/java/avd/inf/jdm/rentmycar/domain/BookingTest.java
@@ -0,0 +1,44 @@
+package avd.inf.jdm.rentmycar.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+
+@SpringBootTest
+class BookingTest {
+
+    User testOwner1 = new User("Test", "The Owner", "welkom", LocalDate.of(2022,01,01), "test+owner@rentmycar.nl", 100);
+
+    User testCustomer1 = new User("Test", "The Customer", "welkom", LocalDate.of(2022,01,01), "test+customer@rentmycar.nl", 100);
+
+    Car testCar1 = new ICE("AB-12-CD", (short) 2006, "Renault Kangoo", ColorType.BLACK, 200000, 5, testOwner1);
+
+    Offer testOffer1 = new Offer(LocalDateTime.parse("2022-10-10T08:00"), LocalDateTime.parse("2022-10-10T10:00"), "Rotterdam", testCar1);
+
+    @Test
+    @DisplayName("Offer can not be set null on initialization")
+    void Booking_OfferCanNotBeNullOnInitialization_ExceptionThrown() {
+        assertThrows(NullPointerException.class, () -> new Booking(null, testCustomer1));
+    }
+
+    @Test
+    @DisplayName("Customer can not be set null on initialization")
+    void Booking_CustomerCanNotBeNullOnInitialization_ExceptionThrown() {
+        assertThrows(NullPointerException.class, () -> new Booking(testOffer1, null));
+    }
+
+    @Test
+    @DisplayName("Default BookingStatus is PENDING")
+    void Booking_DefaultBookingStatusIsPENDING() {
+        Booking testBooking1 = new Booking(testOffer1, testCustomer1);
+        assertEquals(BookingStatus.PENDING, testBooking1.getStatus());
+    }
+
+}

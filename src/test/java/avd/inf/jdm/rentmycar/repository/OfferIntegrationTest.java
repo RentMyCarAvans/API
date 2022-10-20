83c21a52b01f599b7b101800bfc98d4cd54346c1
Roy Schrauwen <roy@royschrauwen.nl>
2022-10-20 08:26:26 +0200

    [81] Added two integrationtests for Offer

---
+++ src/test/java/avd/inf/jdm/rentmycar/repository/OfferIntegrationTest.java
@@ -0,0 +1,61 @@
+package avd.inf.jdm.rentmycar.repository;
+
+import avd.inf.jdm.rentmycar.domain.*;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class OfferIntegrationTest {
+
+
+    User testOwner1 = new User("Tessy", "De Tester", "welkom", LocalDate.of(2022,01,01), "tessy@avans.nl", 100);
+
+    Car testCar1 = new ICE("AB-12-CD", (short) 2006, "Renault Kangoo", ColorType.BLACK, 200000, 5, testOwner1);
+    Offer testOffer1 = new Offer(LocalDateTime.parse("2022-10-10T08:00"), LocalDateTime.parse("2022-10-10T10:00"), "Rotterdam", testCar1);
+
+
+    @Autowired OfferRepository offerRepository;
+    @Autowired UserRepository userRepository;
+    @Autowired CarRepository carRepository;
+
+
+    @BeforeAll
+    public void setup() {
+        userRepository.save(testOwner1);
+        carRepository.save(testCar1);
+    }
+
+    @Test
+    @DisplayName("Offer is added to the database")
+    void Offer_AddingOfferToTheRepository_CountAfterSavingShouldBeOneMore() {
+        int count = offerRepository.findAll().size();
+        offerRepository.save(testOffer1);
+        assertEquals(count + 1, offerRepository.findAll().size());
+    }
+
+    @Test
+    @DisplayName("Offer is removed from the database")
+    void Offer_RemovingOfferFromTheRepository_CountAfterDeletingShouldBeOneLess() {
+        offerRepository.save(testOffer1);
+        int count = offerRepository.findAll().size();
+        offerRepository.delete(testOffer1);
+        assertEquals(count - 1, offerRepository.findAll().size());
+    }
+
+
+
+
+}

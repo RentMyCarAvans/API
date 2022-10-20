07b0b2361546ff5924d16a05968232fa8748701e
Roy Schrauwen <roy@royschrauwen.nl>
2022-10-20 12:33:40 +0200

    [40] Added OpenApi / Swagger documentation

--- src/main/java/avd/inf/jdm/rentmycar/controller/AuthController.java
+++ src/main/java/avd/inf/jdm/rentmycar/controller/AuthController.java
@@ -4,6 +4,11 @@ import avd.inf.jdm.rentmycar.ResponseHandler;
 import avd.inf.jdm.rentmycar.controller.dto.AuthDTO;
 import avd.inf.jdm.rentmycar.domain.User;
 import avd.inf.jdm.rentmycar.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,7 +20,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-@Tag(name = "auth-controller", description = "Endpoints regarding user authentication")
+@Tag(name = "Authentication Controller", description = "Endpoints regarding user authentication")
 @RestController
 @RequestMapping("/api")
 @CrossOrigin
@@ -28,6 +33,10 @@ public class AuthController {
         this.userService = userService;
     }
 
+    @Operation(summary = "Login to Rent My Car")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Successfully logged in",content = { @Content(mediaType = "application/json", schema = @Schema(implementation = AuthDTO.class)) }),
+        @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content) })
     @PostMapping("/v1/auth/login")
     public ResponseEntity<Object> login(
             @RequestBody(required = true) AuthDTO authDTO

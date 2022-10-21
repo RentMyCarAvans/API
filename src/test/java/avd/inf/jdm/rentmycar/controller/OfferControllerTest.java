bef2ec86bec6fcf0d3db26651a6cf9d7283fed5e
Roy Schrauwen <roy@royschrauwen.nl>
2022-10-21 16:36:35 +0200

    [109] Created /offers/5 endpoint test

--- src/test/java/avd/inf/jdm/rentmycar/controller/OfferControllerTest.java
+++ src/test/java/avd/inf/jdm/rentmycar/controller/OfferControllerTest.java
@@ -1,61 +1,46 @@
 package avd.inf.jdm.rentmycar.controller;
 
-import avd.inf.jdm.rentmycar.domain.Offer;
-import avd.inf.jdm.rentmycar.repository.CarRepository;
-import avd.inf.jdm.rentmycar.repository.OfferRepository;
-import avd.inf.jdm.rentmycar.repository.UserRepository;
-import avd.inf.jdm.rentmycar.service.CarService;
-import avd.inf.jdm.rentmycar.service.OfferService;
-import org.junit.jupiter.api.BeforeEach;
+import avd.inf.jdm.rentmycar.RentMyCarApplication;
+import org.json.JSONException;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import java.util.HashMap;
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-@SpringBootTest
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
-class OfferControllerTest {
-
-    OfferController offerController;
-
-    @Autowired OfferService offerService;
-    @Autowired CarService carService;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 
 
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = RentMyCarApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class OfferControllerTest {
+    @LocalServerPort
+    private int port;
 
-    @BeforeEach
-    void setUp(){
-        offerController = new OfferController(offerService, carService);
-    }
+    final TestRestTemplate restTemplate = new TestRestTemplate();
+
+    final HttpHeaders headers = new HttpHeaders();
 
     @Test
-    void shouldReturnAllOffers()  {
-        // arrange
+    public void testRetrieveStudentCourse() throws JSONException {
 
-        // act
-        var result = offerController.getAllOffers(null, null, null);
-        var data = (List<Object>) ((HashMap) result.getBody()).get("data");
+        HttpEntity<String> entity = new HttpEntity<>(null, headers);
 
-        System.out.println(data.size() + " offers found: ");
-        data.forEach(offer -> System.out.println(offer));
+        ResponseEntity<String> response = restTemplate.exchange(
+                createURLWithPort(),
+                HttpMethod.GET, entity, String.class);
 
-        // assert
-        assertEquals(data.size(), data.size());
+        String expected = "{\"data\":{\"id\":5,\"startDateTime\":\"2022-11-01T12:00:00\",\"endDateTime\":\"2022-11-01T13:00:00\",\"pickupLocation\":\"Tilburg\",\"car\":{\"type\":\"BEV\",\"id\":2,\"licensePlate\":\"2BEV34\",\"yearOfManufacture\":2021,\"model\":\"Lamborgini Diablo\",\"colorType\":\"BLACK\",\"mileage\":1000,\"numberOfSeats\":2,\"user\":{\"id\":4,\"email\":\"paul@avans.nl\",\"firstName\":\"Paul\",\"lastName\":\"de Mast\",\"dateOfBirth\":\"2022-10-21\",\"bonusPoints\":100,\"adult\":false,\"admin\":false}}},\"status\":200}";
+
+        JSONAssert.assertEquals(expected, response.getBody(), false);
     }
 
-    @Test
-    void shouldReturnOfferWithValidId() {
-        // arrange
-        var offer = offerController.getById(1L);
-        System.out.println(offer);
-
-        assertNotNull(offer);
+    private String createURLWithPort() {
+        return "http://localhost:" + port + "/api/v1/offers/5";
     }
-
-
 }

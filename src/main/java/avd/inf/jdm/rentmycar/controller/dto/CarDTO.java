f9dbdf877886f8d691b76bbd8449a197893e3fa3
aubreypolderman <92875359+aubreypolderman@users.noreply.github.com>
2022-10-24 18:58:00 +0200

    removed logfactory from cardto

--- src/main/java/avd/inf/jdm/rentmycar/controller/dto/CarDTO.java
+++ src/main/java/avd/inf/jdm/rentmycar/controller/dto/CarDTO.java
@@ -1,16 +1,12 @@
 package avd.inf.jdm.rentmycar.controller.dto;
 
 import avd.inf.jdm.rentmycar.domain.ColorType;
-import avd.inf.jdm.rentmycar.service.CarService;
 import lombok.Getter;
 import lombok.Setter;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @Getter
 @Setter
 public class CarDTO {
-    private static final Logger log = LoggerFactory.getLogger((CarService.class));
     private String type;
     private String licensePlate;
     private Short yearOfManufacture;
